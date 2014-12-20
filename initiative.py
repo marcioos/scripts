4968e138a40da057865a07bded9cde04f422d00f
Marcio Oliveira <marcio.oliveira@thoughtworks.com>
2014-12-19 22:48:20 -0200

    floating point initiative

--- initiative.py
+++ initiative.py
@@ -3,6 +3,8 @@
 from itertools import cycle
 import operator
 import re
+import signal
+import sys
 
 class InitiativeOrder:
 
@@ -16,19 +18,19 @@ class InitiativeOrder:
     self.sorted_initiatives = sorted(initiatives.items(), key = operator.itemgetter(1), reverse = True)
     self.initiative_cycle = cycle(range(len(self.sorted_initiatives)))
 
-  def print_next_combatant(self):
-    print "\nNow it's %s's turn\n" % self.sorted_initiatives[self.initiative_cycle.next()][0]
+  def next_combatant(self):
+    return self.sorted_initiatives[self.initiative_cycle.next()][0]
 
   @staticmethod
   def _map_combatant_entry(raw_combatant):
     combatant_entry = {}
-    re_matcher = re.match(r'^(.+) (\d+) (\d+)$', raw_combatant)
+    re_matcher = re.match(r'^(.+) (\d+) ([0-9]+\.?[0-9]+)$', raw_combatant)
     if re_matcher:
       for i in range(int(re_matcher.group(2))):
-        combatant_entry[re_matcher.group(1) + str(i + 1)] = int(re_matcher.group(3))
+        combatant_entry["%s %s" % (re_matcher.group(1), str(i + 1))] = float(re_matcher.group(3))
     else:
-      re_matcher = re.match(r'^(.+) (\d+)$', raw_combatant)
-      combatant_entry[re_matcher.group(1)] = int(re_matcher.group(2))
+      re_matcher = re.match(r'^(.+) ([0-9]+\.?[0-9]+)$', raw_combatant)
+      combatant_entry[re_matcher.group(1)] = float(re_matcher.group(2))
     return combatant_entry
 
 def read_input():
@@ -41,8 +43,13 @@ def read_input():
       break
   return combatants
 
-initiative_order = InitiativeOrder(read_input())
+def sigint_handler(signal, frame):
+  print "\n\nBye :)\n"
+  sys.exit(0)
 
-while True:
-  initiative_order.print_next_combatant()
-  raw_input("press Enter for next")
+if __name__ == '__main__':
+  signal.signal(signal.SIGINT, sigint_handler)
+  initiative_order = InitiativeOrder(read_input())
+  while True:
+    print "\nNow it's %s's turn\n" % initiative_order.next_combatant()
+    raw_input("press Enter for next combatant")
